c2eb463b5cc019499ef390890219a08f38c7c36c
Ruben Smits <ruben.smits@ing.com>
2023-12-04 08:21:19 +0100

    reformat

--- src/main/kotlin/days/year2023/Day4.kt
+++ src/main/kotlin/days/year2023/Day4.kt
@@ -1,24 +1,23 @@
 package days.year2023
 
 import days.Day
-import java.lang.Math.pow
-import kotlin.math.*
 
 fun main() {
     println(Day4().solve())
 }
-class Day4: Day(4, 2023) {
+
+class Day4 : Day(4, 2023) {
 
     fun solve(): Any {
-        val cards = inputList.mapIndexed{i,it->
-            var (winning, mine) = it.substringAfter(':').split("|").map{extraxtAllIntsFromString(it)};
+        val cards = inputList.map {
+            val (winning, mine) = it.substringAfter(':').split("|").map { extraxtAllIntsFromString(it) };
             val inter = winning.intersect(mine).size
-            Card(i, inter, 1)
+            Card(inter, 1)
         }
         cards.forEachIndexed { index, card ->
-            repeat(card.amount){
-                for(intersects in 0 until card.intersects){
-                    cards[index+intersects+1].amount++
+            repeat(card.amount) {
+                for (intersects in 0 until card.intersects) {
+                    cards[index + intersects + 1].amount++
                 }
             }
         }
@@ -26,4 +25,4 @@ class Day4: Day(4, 2023) {
     }
 }
 
-data class Card(val name:Int, val intersects: Int, var amount:Int)
+data class Card(val intersects: Int, var amount: Int)
